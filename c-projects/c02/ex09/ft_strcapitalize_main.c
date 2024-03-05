858a0ba5b590965197d57e1f9aaab92364549562
Felix Hong Ming Tan <feltan@c2r5s12.42singapore.sg>
2024-03-05 16:13:00 +0800

    Fix 42mots issue

--- c-projects/c02/ex09/ft_strcapitalize_main.c
+++ c-projects/c02/ex09/ft_strcapitalize_main.c
@@ -6,7 +6,7 @@
 /*   By: feltan <feltan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:09:01 by felixtanhm        #+#    #+#             */
-/*   Updated: 2024/03/05 14:38:37 by feltan           ###   ########.fr       */
+/*   Updated: 2024/03/05 16:12:34 by feltan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 int	ft_is_alphanum(char c)
 {
 	if (c < '0')
-		return (1);
+		return (0);
 	if (c > '9' && c < 'A')
-		return (1);
+		return (0);
 	if (c > 'Z' && c < 'a')
-		return (1);
+		return (0);
 	if (c > 'z')
-		return (1);
-	return (0);
+		return (0);
+	return (1);
 }
 
-int	ft_checkfirstchar(char *x)
+int	ft_checkfirstchar(char *c)
 {
-	if (ft_is_alphanum(*x) == 0)
+	if (ft_is_alphanum(*c) == 1)
 	{
-		if (x[0] >= 'a' && x[0] <= 'z')
-			*x -= 32;
+		if (c[0] >= 'a' && c[0] <= 'z')
+			*c -= 32;
 		return (1);
 	}
 	return (0);
@@ -45,12 +45,16 @@ char	*ft_strcapitalize(char *str)
 	is_prior_alphanum = ft_checkfirstchar(&str[0]);
 	while (str[i] != '\0')
 	{
-		if (ft_is_alphanum(str[i]) == 0)
+		if (ft_is_alphanum(str[i]) == 1)
 		{
-			if (is_prior_alphanum == 0 && str[i] >= 'a' && str[i] <= 'z')
+			if(is_prior_alphanum == 0)
 			{
-				str[i] -= 32;
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] -= 32;
+				}
 				is_prior_alphanum = 1;
+
 			}
 		}
 		else
