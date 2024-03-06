a172f34cceb5b5d16d973382f048cde2586091ad
felixtanhm <felixtanhm@gmail.com>
2024-03-06 16:19:34 +0800

    c04 ex03

--- c-projects/c04/ex03/ft_atoi_main.c
+++ c-projects/c04/ex03/ft_atoi_main.c
@@ -1,14 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atoi_main.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: felixtanhm <felixtanhm@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 10:40:36 by feltan            #+#    #+#             */
-/*   Updated: 2024/03/06 16:15:24 by felixtanhm       ###   ########.fr       */
+/*   Updated: 2024/03/06 16:18:45 by felixtanhm       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int whitespace(char *str, int *ptr)
 {
@@ -45,4 +48,14 @@ int ft_atoi(char *str)
 	}
 	result *= sign;
 	return result;
+}
+
+int main(void)
+{
+	char str[] = "    ---+-+-12ab567";
+	int res = ft_atoi(str);
+	int res2 = atoi(str);
+	printf("Res1: %d\n", res);
+	printf("Res2: %d\n", res2);
+	return 0;
 }
