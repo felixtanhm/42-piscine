9c6cbe8d39c72a53be5e89181800a64709510336
Felix Hong Ming Tan <feltan@c2r6s13.42singapore.sg>
2024-03-17 15:20:21 +0800

    ex11 modification

--- c-projects/c02/ex11/ft_putstr_non_printable_main.c
+++ c-projects/c02/ex11/ft_putstr_non_printable_main.c
@@ -1,16 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*   ft_putstr_non_printable_main.c                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: feltan <feltan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:59:54 by feltan            #+#    #+#             */
-/*   Updated: 2024/03/17 15:17:02 by feltan           ###   ########.fr       */
+/*   Updated: 2024/03/17 15:16:53 by feltan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void ft_putchar(char c)
 {
@@ -41,4 +42,13 @@ void ft_putstr_non_printable(char *str)
 		}
 		i++;
 	}
+}
+
+int main(void)
+{
+	ft_putstr_non_printable("Coucou\ntu vas bien");
+	ft_putchar('\n');
+	ft_putstr_non_printable("Coucou tu vas bie\t\v\0n");
+	ft_putchar('\n');
+	ft_putstr_non_printable("");
 }
