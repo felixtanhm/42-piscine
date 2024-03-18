8cd6505171f2a7097b45d838eb7266d236d00cd4
Felix Hong Ming Tan <feltan@c2r6s13.42singapore.sg>
2024-03-18 19:27:45 +0800

    added ex05

---
+++ c-projects/c07/ex05/ft_split.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feltan <feltan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 15:54:12 by feltan            #+#    #+#             */
+/*   Updated: 2024/03/14 09:51:35 by feltan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int is_separator(char c, char *charset)
+{
+	int i = 0;
+
+	while(charset[i] != '\0')
+	{
+		if(c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int ft_strlen_sep(char *str, char *charset)
+{
+	int i = 0;
+
+	while(str[i] != '\0' && !is_separator(str[i], charset))
+		i++;
+	return (i);
+}
+
+int count_words(char *str, char *charset)
+{
+	int count = 0;
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		while(is_separator(str[i], charset))
+			i++;
+		if(str[i] != '\0')
+		{
+			count++;
+			while(!is_separator(str[i], charset) && str[i])
+				i++;
+		}
+	}
+	return count;
+}
+
+char *add_word(char *str, int len)
+{
+	int i;
+	char *word;
+
+	i = 0;
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	while(i < len)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+
+	return word;
+}
+
+char **ft_split(char *str, char *charset)
+{
+	char **strings;
+	int i = 0;
+	int j = 0;
+	int word_len;
+	int word_count = count_words(str, charset);
+
+	strings = (char **)malloc(sizeof(char *) * (word_count + 1));
+	while(str[i] != '\0')
+	{
+		while(is_separator(str[i], charset))
+			i++;
+		word_len = ft_strlen_sep(&str[i], charset);
+		if(word_len)
+		{
+			strings[j] = add_word(&str[i], word_len);
+			j++;
+		}
+		i += word_len;
+		word_len = 0;
+	}
+
+	strings[i] = 0;
+	return strings;
+}
+
