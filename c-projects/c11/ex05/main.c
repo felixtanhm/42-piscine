3116c1f4d721f1aafec2d3791a916b4d1012625f
Felix Hong Ming Tan <feltan@c2r6s13.42singapore.sg>
2024-03-20 14:55:17 +0800

    account for * symbol

--- c-projects/c11/ex05/main.c
+++ c-projects/c11/ex05/main.c
@@ -6,7 +6,7 @@
 /*   By: feltan <feltan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:16:06 by feltan            #+#    #+#             */
-/*   Updated: 2024/03/20 11:47:13 by feltan           ###   ########.fr       */
+/*   Updated: 2024/03/20 14:53:06 by feltan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc < 4)
+	if(argc != 4)
 		return 1;
 	ft_op_exec(argv[1], argv[3], argv[2]);
 	return 0;
